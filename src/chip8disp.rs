eb8ae9c0ff0e779f9b3298bc508230a848c24739
Abhinav Tripathi <tripathi_a@worksap.co.jp>
2019-08-19 15:35:34 +0900

    Make the basic classes that will be used

---
+++ src/chip8disp.rs
@@ -0,0 +1,19 @@
+struct Chip8Display {
+    pixels: [[bool; 64]; 32]
+}
+
+impl Chip8Display {
+    fn disp_clear(&mut self) -> bool {
+        // clear the display, return true if any on pixel was switched to off
+        let mut collision = false;
+        for (x, row) in self.pixels.iter_mut().enumerate() {
+            for (y, col) in row.iter_mut().enumerate() {
+                if col == true {
+                    collision = true;
+                }
+                *col = false;
+            }
+        }
+        collision
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+extern crate byteorder;
+use byteorder::{ReadBytesExt, BigEndian};
+
+use std::fs::File;
+use std::io::{BufReader, Result};
+
+fn main() -> Result<()> {
+    let args: Vec<String> = std::env::args().collect();
+    let file_name = args[1].clone();
+    println!("Attempting to read: {}", file_name);
+
+    let file = File::open(file_name).unwrap();
+    let length = file.metadata().unwrap().len() as usize;
+    println!("Filesize: {} bytes", length);
+    // Number of u16 values is length/2
+    let mut buffer = vec![0u8; length/2];
+    let mut buf_reader = BufReader::new(file);
+    // CHIP-8 file is BigEndian u16 values
+    buf_reader.read_u16_into::<BigEndian>(&mut buffer[..]).expect("Failed to read file!");
+    println!("buffer: {:?}", buffer);
+    for word in buffer {
+        print!("{:x}, ", word);
+    }
+
+    Ok(())
 }
