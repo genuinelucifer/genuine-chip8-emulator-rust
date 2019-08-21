fbd4b9c4abdbaf454ea26b99822a2786da08dca9
Ravi Kumar <rdahiya0213@gmail.com>
2019-08-21 16:54:12 +0900

    implement opcode 0xE000 input handler

--- src/chip8/display/mod.rs
+++ src/chip8/display/mod.rs
@@ -33,7 +33,6 @@ impl Chip8Display {
 
     pub fn draw_byte(&mut self, sprite: u8, row: usize, column: usize) -> bool {
         let mut collision = false;
-        println!("invoked draw_byte byte: {} row: {} col: {}", sprite, row, column);
         for i in 0..8 {
             let _row= row%32 as usize;
             let _column = (column+i)%64 as usize;
@@ -43,35 +42,28 @@ impl Chip8Display {
                     collision = true;
                 }
                 self.pixels[_row][_column] ^= true;
-                println!("pixel at coord {} {} : {}", _row, _column,self.pixels[_row][_column] );
             }
         }
         collision
     }
 
     pub fn update(&mut self) {
-        println!("invoked update");
         let mut flag = false;
         while let Some(e) = self.window.next() {
             if flag {
                 break;
             }
-            println!("invoked update event");
             if let Some(r) = e.render_args() {
                 for (x, row) in self.pixels.iter().enumerate() {
                     for (y, col) in row.iter().enumerate() {
                         if *col == true {
-                            println!("{} {} {}", x, y, *col);
                             self.window.draw_2d(&e, |c, g, _device| {
-                                println!("drawing pixel at x: {}, y: {}", x, y);
                                 rectangle([255.0, 255.0, 255.0, 1.0], // white
                                           [(y*10) as f64, (x*10) as f64, 10.0, 10.0],
                                           c.transform, g);
                             });
-                            println!("post {} {} {}", x, y, *col);
                         } else {
                             self.window.draw_2d(&e, |c, g, _device| {
-                                println!("erasing pixel at x: {}, y: {}", x, y);
                                 rectangle([0.0, 0.0, 0.0, 1.0], // black
                                           [(y*10) as f64, (x*10) as f64, 10.0, 10.0],
                                           c.transform, g);
@@ -80,9 +72,20 @@ impl Chip8Display {
                         }
                     }
                 }
-
                 flag = true;
             }
         }
+    }
+
+    pub fn get_pressed_key(&mut self) -> Option<Key> {
+        let mut events = Events::new(EventSettings::new().lazy(true));
+        let mut key = None;
+        if let Some(e) = events.next(&mut self.window) {
+            if let Some(Button::Keyboard(k)) = e.press_args() {
+                println!("Pressed keyboard key '{:?}'", k);
+                key = Some(k);
+            }
+        }
+        key
     }
 }
