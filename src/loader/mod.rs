2ecfd61563a88aab73e35ded6422a0030b3da80f
Abhinav Tripathi <tripathi_a@worksap.co.jp>
2019-08-21 17:08:27 +0900

    Sort rom list and minor edits

--- src/loader/mod.rs
+++ src/loader/mod.rs
@@ -3,14 +3,14 @@ use std::io;
 
 pub fn get_roms(path: &String) -> Result<Vec<fs::DirEntry>, io::Error> {
     fs::read_dir(path).and_then(
-        |paths|
-            Ok(
-                paths
+        |paths| {
+            let mut collected_paths = paths
                     .filter(|path|path.is_ok())
                     .map(|path|path.unwrap())
-                    .collect::<Vec<fs::DirEntry>>()
-            )
-
+                    .collect::<Vec<fs::DirEntry>>();
+            collected_paths.sort_by(|a, b| a.file_name().cmp(&b.file_name()));
+            Ok(collected_paths)
+        }
     )
 }
 
