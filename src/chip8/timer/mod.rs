59a72f55b26b02b54803dcdcf7dfdbbf84cb5c8e
Abhinav Tripathi <genuinelucifer@gmail.com>
2019-08-21 13:59:46 +0900

    Implement most opcodes. Leave TODOs where remaining.

--- src/chip8/timer/mod.rs
+++ src/chip8/timer/mod.rs
@@ -2,7 +2,7 @@ extern crate chrono;
 use chrono::prelude::*;
 
 pub struct Chip8Timer {
-    val: u16,
+    value: u8,
     last_time: i64
 }
 
@@ -14,32 +14,32 @@ impl Chip8Timer {
 
     pub fn new() -> Chip8Timer {
         Chip8Timer {
-            val: 0,
+            value: 0,
             last_time: cur_time()
         }
     }
 
-    pub fn update_val(&mut self) {
+    pub fn update_value(&mut self) {
         let cur_time = cur_time();
         let mili_passed = cur_time - self.last_time;
         // Reduce the counter by 60 every second
-        let reduction = (60.0 * (mili_passed as f32 / 1000.00)) as u16;
+        let reduction = (60.0 * (mili_passed as f32 / 1000.00)) as u8;
         if reduction > 0 {
             self.last_time = cur_time;
-            self.val -= reduction;
-            if self.val < 0 {
-                self.val = 0;
+            self.value -= reduction;
+            if self.value < 0 {
+                self.value = 0;
             }
         }
     }
 
-    pub fn get_val(&mut self) -> u16 {
-        self.update_val();
-        self.val
+    pub fn get_value(&mut self) -> u8 {
+        self.update_value();
+        self.value
     }
 
-    pub fn set_val(&mut self, val: u16) {
-        self.val = val;
+    pub fn set_value(&mut self, value: u8) {
+        self.value = value;
         self.last_time = cur_time();
     }
 }
