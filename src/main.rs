ce7e243666ff3455b665739a2cc4794ef68a3583
ravi kumar <rdahiya0213@gmail.com>
2019-08-22 15:32:25 +0900

    handle key events

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+extern crate piston_window;
+use piston_window::*;
+
 mod loader;
 mod chip8;
 
@@ -27,12 +30,69 @@ fn start() -> Result<(), std::io::Error> {
                         let rom_data = loader::load_roms(&roms_with_index[line-1].1.path().to_str());
                         match rom_data {
                             Ok(v) => {
+                                let mut window: PistonWindow = WindowSettings::new("Chip8!", [640, 320]).exit_on_esc(true).automatic_close(true).build().unwrap();
                                 let mut chip8_cpu = chip8::cpu::Chip8CPU::new();
                                 chip8_cpu.load_program(&v);
                                 println!("{:?}", v);
-                                loop {
-                                    chip8_cpu.exec_next_instruction();
+
+                                let mut events = Events::new(EventSettings::new());
+
+                                while let Some(e) = events.next(&mut window) {
+                                    if let Some(Button::Keyboard(key)) = e.press_args() {
+                                        println!("{:?}",key);
+                                        match key {
+                                            Key::NumPad0 => chip8_cpu.set_key(0x0),
+                                            Key::NumPad1 => chip8_cpu.set_key(0x1),
+                                            Key::NumPad2 => chip8_cpu.set_key(0x2),
+                                            Key::NumPad3 => chip8_cpu.set_key(0x3),
+                                            Key::NumPad4 => chip8_cpu.set_key(0x4),
+                                            Key::NumPad5 => chip8_cpu.set_key(0x5),
+                                            Key::NumPad6 => chip8_cpu.set_key(0x6),
+                                            Key::NumPad7 => chip8_cpu.set_key(0x7),
+                                            Key::NumPad8 => chip8_cpu.set_key(0x8),
+                                            Key::NumPad9 => chip8_cpu.set_key(0x9),
+                                            Key::A => chip8_cpu.set_key(0xA),
+                                            Key::B => chip8_cpu.set_key(0xB),
+                                            Key::C => chip8_cpu.set_key(0xC),
+                                            Key::D => chip8_cpu.set_key(0xD),
+                                            Key::E => chip8_cpu.set_key(0xE),
+                                            Key::F => chip8_cpu.set_key(0xF),
+                                            _ => {}
+                                        }
+                                    }
+                                    //if let Some(r) = e.render_args() {
+                                        chip8_cpu.update_display(&mut window, &e);
+                                    //}
+
+                                    //if let Some(u) = e.update_args() {
+                                        chip8_cpu.exec_next_instruction();
+                                    //}
+                                    if let Some(Button::Keyboard(key)) = e.release_args() {
+                                        match key {
+                                            Key::NumPad0 => chip8_cpu.unset_key(0x0),
+                                            Key::NumPad1 => chip8_cpu.unset_key(0x1),
+                                            Key::NumPad2 => chip8_cpu.unset_key(0x2),
+                                            Key::NumPad3 => chip8_cpu.unset_key(0x3),
+                                            Key::NumPad4 => chip8_cpu.unset_key(0x4),
+                                            Key::NumPad5 => chip8_cpu.unset_key(0x5),
+                                            Key::NumPad6 => chip8_cpu.unset_key(0x6),
+                                            Key::NumPad7 => chip8_cpu.unset_key(0x7),
+                                            Key::NumPad8 => chip8_cpu.unset_key(0x8),
+                                            Key::NumPad9 => chip8_cpu.unset_key(0x9),
+                                            Key::A => chip8_cpu.unset_key(0xA),
+                                            Key::B => chip8_cpu.unset_key(0xB),
+                                            Key::C => chip8_cpu.unset_key(0xC),
+                                            Key::D => chip8_cpu.unset_key(0xD),
+                                            Key::E => chip8_cpu.unset_key(0xE),
+                                            Key::F => chip8_cpu.unset_key(0xF),
+                                            _ => {}
+                                        }
+                                    }
                                 }
+
+//                                loop {
+//                                    chip8_cpu.exec_next_instruction();
+//                                }
                             },
                             Err(e) => {
                                 println!("Error occurred while loading rom!");
